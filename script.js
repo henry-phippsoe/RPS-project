9c45dae10675fafef8b657fcbd6c854a6af9a31c
cowlyfe <26hsoeiro@cpsd.us>
2025-05-19 17:09:41 -0400

    human function & var

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-console.log("hello world");
-
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    let choice = Math.floor(Math.random * 3) + 1;
+    let choice = Math.floor(Math.random() * 3) + 1;
     console.log("Computer chose: " + choice);
     return choice;
 }
 
 function getHumanChoice() {
-
+    let choice = prompt("Choose to throw rock, paper, or scissors: ");
+    console.log("You chose: " + choice);
+    return choice;
 }
 
 function playRound() {
-
+    
 }
 
 function playGame() {
 
 }
+
+getComputerChoice();
+getHumanChoice();

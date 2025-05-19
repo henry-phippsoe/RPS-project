7eee5c345a450e2ea155b3128e9031fa0a0f9864
cowlyfe <26hsoeiro@cpsd.us>
2025-05-19 17:03:13 -0400

    computer function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("hello world");
+
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random * 3) + 1;
+    console.log("Computer chose: " + choice);
+    return choice;
+}
+
+function getHumanChoice() {
+
+}
+
+function playRound() {
+
+}
+
+function playGame() {
+
+}
